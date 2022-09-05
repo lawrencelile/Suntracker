997998f32b26a4d52e72c82486f7508059559673
lawrencelile <lawrencelile@gmail.com>
2022-09-05 16:36:56 +0100

    Update readme.md

---
+++ solartracker.py
@@ -0,0 +1,17 @@
+# solartracker.py
+# by Lawrence Lile
+#
+#Logging for troubleshooting
+import logging
+logging.basicConfig(filename='solartracker.log', encoding='utf8', level=logging.DEBUG)
+#logging.debug('this is debug message')
+#logging.info('this is imformation message')
+#logging.warning('This is warning message')
+#logging.error('This is error message')
+
+
+print("Hello, this is the solartracker")
+logging.info('Hello, this is the solartrackers first message')
+
+
+
